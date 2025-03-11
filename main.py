e72be2df2155bfe6f4b3ec10a545359fc7cce703
zafarshodmonov1996 <zafarshodmonov1996@gmail.com>
2025-03-11 18:18:41 +0500

    z

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-# save this as app.py
-from flask import Flask
-
+from flask import Flask, request
+import json
+import csv
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/get/data', methods=['POST'])
+def get_data():
+    if request.method == 'POST':
+        data_form = request.form
+        a = data_form.get('a')
+        b = data_form.get('b')
+        return {'a': a, 'b': b}
+    return 'Invalid request'
+
+        
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
